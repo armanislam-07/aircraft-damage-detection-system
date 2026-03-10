50341419a285e0ba37cc7bf7f16c5e2fd1082922
armanislam-07 <armanislamusa@gmail.com>
2026-03-10 12:18:54 -0700

    Added RF Communcation Feature: Including NRF and SPI Drivers

---
+++ onboard_sensor_system/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "nrf24l01.h"
+
+
+int main(void)
+{
+    nrf24_device(TRANSMITTER, RESET);
+    nrf24_rf_power(6);
+    nrf24_rf_channel(40);
+    nrf24_datapipe_ptx(0);
+    nrf24_flush(TX_BUFFER);
+    uint8_t data = 1;
+    nrf24_prx_static_payload_width(1,1);
+
+
+    while(1)
+    {
+
+    nrf24_transmit(&data, 1, NO_ACK_MODE);
+    while(nrf24_transmit_status() == TRANSMIT_IN_PROGRESS);
+    delay_function(100); 
+    }
+    return 0;
+}
