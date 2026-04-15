40d8e3118b5c29926d297805320ec6500dfac85b
Arman Islam <armanislamusa@gmail.com>
2026-04-15 17:15:14 -0400

    Add python scripts for fetching BLE string (through parsing) and data packets
    Parsing UART String over BLE -> logging_resistance_parsing_string.py
    
    Data Packets -> logging_resistance_data_packet.py

---
+++ logging/logging_resistance_data_packet.py
@@ -0,0 +1,35 @@
+import asyncio
+from bleak import BleakClient
+import struct
+
+#This is the device address of the NRF52840 specific to your current device
+#Change if using a different setup
+DEVICE_ADDRESS = "F8:56:1C:1F:C6:2D"
+
+# Nordic UART Service UUID (standard for BLEUart)
+UART_RX_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+log_file = "ble_log.txt"
+buffer =""
+
+def callback(sender, data):
+    if len(data) == 12:
+        r, v, i = struct.unpack("fff", data)
+        line = f"R:{r:.3f},V:{v:.3f},I:{i:.3f}"
+
+        print(line)
+        with open(log_file, "a") as f:
+            f.write(line + "\n")
+
+async def main():
+    async with BleakClient(DEVICE_ADDRESS) as client:
+        print("Connected!")
+
+        await client.start_notify(UART_RX_UUID, callback)
+
+        print("Logging started")
+
+        while True:
+            await asyncio.sleep(1)
+
+asyncio.run(main())
