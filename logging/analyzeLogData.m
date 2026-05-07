42a2ee0b22a389b63073689fdde6304259eeaf0e
Arman Islam <armanislamusa@gmail.com>
2026-05-07 19:39:42 -0400

    Add final version of MATLAB log code analyzer script

--- logging/analyzeLogData.m
+++ logging/analyzeLogData.m
@@ -6,12 +6,20 @@ timeinMS = data(:,1);
 resistance = data(:, 2);
 voltage = data(:, 3); % Currently no use 
 current = data(:, 4); % Currently no use 
-time = timeinMS / 1000;
+time = timeinMS / 1000; % Converts our time into seconds
 
-%(Add section of code to truncate data values we won't use)
-%Truncate before some time 
-%Truncate After some time
-
+%% TO TRUNCATE WHAT WE WANT TO DISPLAY
+% i = find(time > VALUE_WE_WANT_TIME_TO_START)
+% To truncate the beginning
+% time = time(i:end)
+% resistance = resistance(i:end)
+%
+% To truncate an end
+% time = time(1:endValue)
+% resistance = resistance(1:endValue)
+%%%% We can also utilize MATLABS for the resistance too | For instance: I want to cut
+%%%% out where the resistance is 0 since that is when we plugged the
+%%%% battery out etc. 
 
 % Figure that shows our resistance over time
 figure(1)
